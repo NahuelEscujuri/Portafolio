7c56453c01919b4cad37615822e9c057ed7d57c7
Nahuel <nahuelescujuri@gmail.com>
2024-02-08 20:42:15 -0300

    fix Next 1

--- pages/_app.js
+++ pages/_app.js
@@ -4,14 +4,9 @@ import Head from 'next/head';
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Nahuel Escujuri</title>
-	      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
-        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-        <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet"/>
-      </Head>
+      {/* <Head>
+        
+      </Head> */}
       <Component {...pageProps} />
     </>
   )
