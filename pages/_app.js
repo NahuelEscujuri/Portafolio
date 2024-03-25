fe9ced598f6cab6fd2e2e33a7c79c48319cf1657
Nahuel <nahuelescujuri@gmail.com>
2024-03-25 11:38:20 -0300

    librerias de vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import Head from 'next/head';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App({ Component, pageProps }) {
   return (
@@ -13,6 +14,7 @@ export default function App({ Component, pageProps }) {
         {/* <link href="https://fonts.googleapis.com/css2?family=Inconsolata:wght@300&display=swap" rel="stylesheet"/> */}
       </Head>
       <Component {...pageProps} />
+      <SpeedInsights/>
     </>
   )
 }
